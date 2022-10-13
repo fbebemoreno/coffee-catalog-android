01eec4c40d0b4a2bf61b78964267eca755d6e00f
Fozan Bebe Moreno <fozbebemoreno@gmail.com>
2022-10-13 10:34:45 +0700

    Menambahkan bottom navbar

--- app/src/main/java/unj/cs/hw4/MainActivity.kt
+++ app/src/main/java/unj/cs/hw4/MainActivity.kt
@@ -2,10 +2,16 @@ package unj.cs.hw4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import unj.cs.hw4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
