d99fc0cb78ec1410e81bb520888c90e6d0a3d405
Fozan Bebe Moreno <fozbebemoreno@gmail.com>
2022-10-14 08:19:41 +0700

    fitur pindah fragment dari bottom navigation

--- app/src/main/java/unj/cs/hw4/MainActivity.kt
+++ app/src/main/java/unj/cs/hw4/MainActivity.kt
@@ -2,6 +2,8 @@ package unj.cs.hw4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import unj.cs.hw4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,22 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(CoffeeListFragment())
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.mi_coffee_list -> replaceFragment(CoffeeListFragment())
+                R.id.mi_favourite_list -> replaceFragment(FavouriteListFragment())
+                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
